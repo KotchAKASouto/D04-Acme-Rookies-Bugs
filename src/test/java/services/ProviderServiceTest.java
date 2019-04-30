d9fd04a79279427987702339a1ecb961c764b5e6
Santiago Borge Sanchez <santiagoborgesanchez@gmail.com>
2019-04-30 09:32:26 +0200

    ProviderServiceTest (solo la clase)

---
+++ src/test/java/services/ProviderServiceTest.java
@@ -0,0 +1,32 @@
+
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import utilities.AbstractTest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class ProviderServiceTest extends AbstractTest {
+
+	//The SUT----------------------------------------------------
+	@Autowired
+	private ProviderService	providerService;
+
+	/*
+	 * ----CALCULATE COVERAGE----
+	 * The previous delivery, we calculate it manually. In this one instead we are using the plugin called EclEmma,
+	 * with which we can automatically calculate the percentage.
+	 * 
+	 * Each of the test have their result just before them, and the coverage of the complete test is shown at the end of the document.
+	 */
+
+}
